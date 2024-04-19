aa146ce219e6805f4f23b456a6d9a2a0c4c806c7
mogana varshini <92911280+Mogana004@users.noreply.github.com>
2024-04-19 22:45:48 +0530

    Delete 3rightRotateArray.py

--- 3rightRotateArray.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        length = len(nums)
-        k = k % length 
-        nums[:] = nums[-k:] + nums[:-k]  # Modify the original nums list in-place
